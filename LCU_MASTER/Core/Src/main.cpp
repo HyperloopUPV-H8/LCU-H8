e6214ffc64978f42d3604defbae5ef20817f3dba
Stefan Costea <stefanxenadu@gmail.com>
2023-05-23 18:28:49 +0200

    LCU_MASTER added

---
+++ LCU_MASTER/Core/Src/main.cpp
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "lwip.h"
+
+#include "ST-LIB.hpp"
+#include "Runes/Runes.hpp"
+#include "LCU_MASTER.hpp"
+
+int main(void)
+{
+	STLIB::start();
+
+	while(1) {
+		STLIB::update();
+	}
+}
+
+void Error_Handler(void)
+{
+	ErrorHandler("HAL error handler triggered");
+	while (1){}
+}
---
+++ LCU_MASTER/build/CMakeFiles/LCU_MASTER.elf.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LCU_MASTER.elf.dir/Core/Src/main.cpp.obj"
+  "CMakeFiles/LCU_MASTER.elf.dir/Core/Src/main.cpp.obj.d"
+  "CMakeFiles/LCU_MASTER.elf.dir/Core/Src/stm32h7xx_hal_msp.c.obj"
+  "CMakeFiles/LCU_MASTER.elf.dir/Core/Src/stm32h7xx_hal_msp.c.obj.d"
+  "CMakeFiles/LCU_MASTER.elf.dir/Core/Src/stm32h7xx_it.c.obj"
+  "CMakeFiles/LCU_MASTER.elf.dir/Core/Src/stm32h7xx_it.c.obj.d"
+  "CMakeFiles/LCU_MASTER.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/LCU_MASTER.elf.dir/Core/Src/syscalls.c.obj.d"
+  "CMakeFiles/LCU_MASTER.elf.dir/Core/Src/sysmem.c.obj"
+  "CMakeFiles/LCU_MASTER.elf.dir/Core/Src/sysmem.c.obj.d"
+  "CMakeFiles/LCU_MASTER.elf.dir/Core/Src/system_stm32h7xx.c.obj"
+  "CMakeFiles/LCU_MASTER.elf.dir/Core/Src/system_stm32h7xx.c.obj.d"
+  "CMakeFiles/LCU_MASTER.elf.dir/Core/Startup/startup_stm32h723zgtx.s.obj"
+  "LCU_MASTER.elf"
+  "LCU_MASTER.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C CXX)
+  include(CMakeFiles/LCU_MASTER.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
